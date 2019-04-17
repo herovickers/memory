97942f06b58e3578307a7be0112419a05df05783
herovickers <victoreronmosele@gmail.com>
2019-04-17 17:30:38 +0100

    Switch themes dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:memory/blocs/status_bar_color_bloc.dart';
 import 'package:memory/ui/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -7,18 +9,100 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    ThemeData lightThemeData = ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.grey,
+      primaryColor: Colors.grey[50],
+      textTheme: theme.textTheme.copyWith(
+        headline: theme.textTheme.headline.copyWith(
+          color: Colors.black,
+        ),
+        display1: theme.textTheme.headline.copyWith(
+          color: Colors.black38,
+        ),
+        //This is just title with another color
+        display2: theme.textTheme.title.copyWith(
+          color: Colors.black54,
+        ),
+        display3: theme.textTheme.title.copyWith(
+          color: Colors.black87,
+        ),
+        caption: theme.textTheme.caption.copyWith(color: Colors.black38),
+        button: theme.textTheme.button.copyWith(color: Colors.black),
+        subhead: theme.textTheme.subhead.copyWith(color: Colors.black38),
+        subtitle: theme.textTheme.subtitle.copyWith(color: Colors.black38),
+      ),
+      iconTheme: theme.iconTheme.copyWith(color: Colors.black38),
+    );
+
+    ThemeData darkThemeData = ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.grey[850],
+
+      // primarySwatch: Colors.grey[850],
+      textTheme: theme.textTheme.copyWith(
+        headline: theme.textTheme.headline.copyWith(
+          color: Colors.white,
+        ),
+        display1: theme.textTheme.headline.copyWith(
+          color: Colors.white30,
+        ),
+        //This is just title with another color
+        display2: theme.textTheme.title.copyWith(
+          color: Colors.white54,
+        ),
+
+        display3: theme.textTheme.title.copyWith(
+          color: Colors.white70,
+        ),
+        caption: theme.textTheme.caption.copyWith(color: Colors.white30),
+        button: theme.textTheme.button.copyWith(color: Colors.white),
+        subhead: theme.textTheme.subhead.copyWith(color: Colors.white30),
+        subtitle: theme.textTheme.subtitle.copyWith(color: Colors.white30),
+      ),
+      iconTheme: theme.iconTheme.copyWith(color: Colors.white30),
+    );
+
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
-    return MaterialApp(
-      title: 'Memory',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
+    return DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) =>
+            returnCurrentTheme(brightness, darkThemeData, lightThemeData),
+        themedWidgetBuilder: (context, theme) {
+          return MaterialApp(
+            title: 'Memory',
+            debugShowCheckedModeBanner: false,
+            theme: theme,
+            home: HomePage(),
+          );
+        });
+  }
+
+  ThemeData returnCurrentTheme(Brightness brightness, ThemeData darkThemeData,
+      ThemeData lightThemeData) {
+    if (brightness == Brightness.dark) {
+      statusBarColorBloc.setBrightness(darkThemeData.primaryColor);
+
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarColor:
+              statusBarColorBloc.brightnessValue, // status bar color
+        ),
+      );
+      return darkThemeData;
+    } else {
+      statusBarColorBloc.setBrightness(lightThemeData.primaryColor);
+
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark,
+        statusBarColor: statusBarColorBloc.brightnessValue, // status bar color
+      ));
+
+      return lightThemeData;
+    }
   }
 }
-
- 
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:memory/blocs/status_bar_color_bloc.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({
@@ -11,6 +14,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarIconBrightness: Theme.of(context).brightness == Brightness.light
+          ? Brightness.dark
+          : Brightness.light,
+      // statusBarColor: Colors.grey[500], // status bar color
+      statusBarColor: statusBarColorBloc.brightnessValue, // status bar color
+    ));
     return Scaffold(
       body: Center(
         child: Column(
@@ -22,6 +32,27 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+          icon: Icon(Icons.swap_calls),
+          label: Text("Switch theme"),
+          onPressed: () {
+            setState(() {
+              switchBrightness();
+            });
+          }),
     );
+  }
+
+  switchBrightness() {
+    print(Theme.of(context).brightness.toString());
+
+    if (Theme.of(context).brightness == Brightness.dark) {
+      DynamicTheme.of(context).setBrightness(Brightness.light);
+      // DynamicTheme.of(context).setThemeData(da)
+      print("set as light");
+    } else {
+      DynamicTheme.of(context).setBrightness(Brightness.dark);
+      print("set as dark");
+    }
   }
 }
